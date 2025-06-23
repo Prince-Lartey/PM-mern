54d8877f8a426736fcae6f432fc8fd3006bddef2
Prince Lartey <lartey.princ@gmail.com>
2025-06-23 17:41:10 +0000

    on it

---
+++ backend/index.js
@@ -0,0 +1,44 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+
+dotenv.config();
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL,
+        methods: ["GET", "POST", "DELETE", "PUT"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+app.use(morgan("dev"));
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("BD Connected successfully."))
+  .catch((err) => console.log("Failed to connect to DB:", err));
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+// error middleware
+app.use((err, req, res, next) => {
+    console.log(err.stack);
+    res.status(500).json({ message: "Internal server error" });
+});
+
+// not found middleware
+app.use((req, res) => {
+    res.status(404).json({
+        message: "Not found",
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
