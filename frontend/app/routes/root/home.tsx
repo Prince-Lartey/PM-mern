32ee2ef18f7e75eafc4aa2603a8003adfe90dff4
Prince Lartey <lartey.princ@gmail.com>
2025-06-23 16:08:22 +0000

    sign in

---
+++ frontend/app/routes/root/home.tsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router";
+import type { Route } from "../+types/home";
+import { Button } from "~/components/ui/button";
+
+export function meta({}: Route.MetaArgs) {
+    return [
+        { title: "TeamSync" },
+        { name: "description", content: "Welcome to TeamSync!" },
+    ];
+}
+
+export default function Home() {
+    return (
+        <div className="w-full h-screen flex items-center justify-center gap-4">
+            <Link to="/sign-in">
+                <Button className="bg-blue-500 text-white">Login</Button>
+            </Link>
+            <Link to="/sign-up">
+                <Button variant="outline" className="bg-blue-500 text-white">
+                    Sign Up
+                </Button>
+            </Link>
+        </div>
+    )
+}
