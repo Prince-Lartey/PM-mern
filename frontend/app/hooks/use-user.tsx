1b09f3dbc4e1a0505aa8f9a773b2ae8b2742523b
Prince Lartey <lartey.princ@gmail.com>
2025-07-10 17:23:04 +0000

    final push

---
+++ frontend/app/hooks/use-user.tsx
@@ -0,0 +1,25 @@
+
+import { useMutation, useQuery, type QueryKey } from "@tanstack/react-query";
+import { fetchData, updateData } from "~/lib/fetch-util";
+import type { ChangePasswordFormData, ProfileFormData } from "~/routes/user/profile";
+
+const queryKey: QueryKey = ["user"];
+
+export const useUserProfileQuery = () => {
+    return useQuery({
+        queryKey,
+        queryFn: () => fetchData("/users/profile"),
+    });
+};
+
+export const useChangePassword = () => {
+    return useMutation({
+        mutationFn: (data: ChangePasswordFormData) => updateData("/users/change-password", data),
+    });
+};
+
+export const useUpdateUserProfile = () => {
+    return useMutation({
+        mutationFn: (data: ProfileFormData) => updateData("/users/profile", data),
+    });
+};
--- frontend/app/routes/user/profile.tsx
+++ frontend/app/routes/user/profile.tsx
@@ -1,5 +1,22 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { AlertCircle, Loader, Loader2 } from 'lucide-react';
 import React from 'react'
+import { FormProvider, useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
+import { toast } from 'sonner';
 import { z } from 'zod';
+import { BackButton } from '~/components/back-button';
+import { Alert, AlertDescription } from '~/components/ui/alert';
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar';
+import { Button } from '~/components/ui/button';
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '~/components/ui/card';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '~/components/ui/form';
+import { Input } from '~/components/ui/input';
+import { Label } from '~/components/ui/label';
+import { Separator } from '~/components/ui/separator';
+import { useChangePassword, useUpdateUserProfile, useUserProfileQuery } from '~/hooks/use-user';
+import { useAuth } from '~/provider/auth-context';
+import type { User } from '~/types';
 
 const changePasswordSchema = z.object({
     currentPassword: z.string().min(1, { message: "Current password is required" }), newPassword: z.string().min(8, { message: "New password is required" }),
@@ -20,9 +37,274 @@ export type ChangePasswordFormData = z.infer<typeof changePasswordSchema>;
 export type ProfileFormData = z.infer<typeof profileSchema>;
 
 const profile = () => {
-    
+    const { data: user, isPending } = useUserProfileQuery() as {
+        data: User;
+        isPending: boolean;
+    };
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const form = useForm<ChangePasswordFormData>({
+        resolver: zodResolver(changePasswordSchema),
+        defaultValues: {
+            currentPassword: "",
+            newPassword: "",
+            confirmPassword: "",
+        },
+    });
+
+    const profileForm = useForm<ProfileFormData>({
+        resolver: zodResolver(profileSchema),
+        defaultValues: {
+            name: user?.name || "",
+            profilePicture: user?.profilePicture || "",
+        },
+        values: {
+            name: user?.name || "",
+            profilePicture: user?.profilePicture || "",
+        },
+    });
+
+    const { mutate: updateUserProfile, isPending: isUpdatingProfile } = useUpdateUserProfile();
+    const { mutate: changePassword, isPending: isChangingPassword, error } = useChangePassword()
+
+    const handlePasswordChange = (values: ChangePasswordFormData) => {
+        changePassword(values, {
+            onSuccess: () => {
+                toast.success("Password updated successfully. You will be logged out. Please login again.");
+                form.reset();
+
+                setTimeout(() => {
+                    logout();
+                    navigate("/sign-in");
+                }, 3000);
+            },
+            onError: (error: any) => {
+                const errorMessage = error.response?.data?.error || "Failed to update password";
+                toast.error(errorMessage);
+                console.log(error);
+            },
+        });
+    };
+
+    const handleProfileFormSubmit = (values: ProfileFormData) => {
+        updateUserProfile(
+            { name: values.name, profilePicture: values.profilePicture || "" },
+            {
+                onSuccess: () => {
+                    toast.success("Profile updated successfully");
+                },
+                onError: (error: any) => {
+                    const errorMessage =
+                        error.response?.data?.error || "Failed to update profile";
+                    toast.error(errorMessage);
+                    console.log(error);
+                },
+            }
+        );
+    };
+
+    if (isPending) {
+        return (
+            <div className="flex justify-center items-center h-screen">
+                <Loader className="animate-spin" />
+            </div>
+        );
+    }
+
     return (
-        <div>profile</div>
+        <div className="space-y-8">
+            <div className="px-4 md:px-0">
+                <BackButton />
+                <h3 className="text-lg font-medium">Profile Information</h3>
+                <p className="text-sm text-muted-foreground">
+                    Manage your account settings and preferences.
+                </p>
+            </div>
+
+            <Separator />
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Personal Information</CardTitle>
+                    <CardDescription>Update your personal details.</CardDescription>
+                </CardHeader>
+
+                 <CardContent>
+                    <FormProvider {...profileForm}>
+                        <form onSubmit={profileForm.handleSubmit(handleProfileFormSubmit)} className="grid gap-4">
+                            <div className="flex items-center space-x-4 mb-6">
+                                <Avatar className="h-20 w-20 bg-gray-600">
+                                    <AvatarImage
+                                        src={
+                                            profileForm.watch("profilePicture") ||
+                                            user?.profilePicture
+                                        }
+                                        alt={user?.name}
+                                    />
+                                    <AvatarFallback className="text-xl">
+                                        {user?.name?.charAt(0) || "U"}
+                                    </AvatarFallback>
+                                </Avatar>
+                                <div>
+                                    <input
+                                        id="avatar-upload"
+                                        type="file"
+                                        accept="image/*"
+                                        // onChange={handleAvatarChange}
+                                        // disabled={uploading || isUpdatingProfile}
+                                        style={{ display: "none" }}
+                                    />
+                                    <Button
+                                        type="button"
+                                        size="sm"
+                                        variant="outline"
+                                        onClick={() =>
+                                            document.getElementById("avatar-upload")?.click()
+                                        }
+                                        // disabled={uploading || isUpdatingProfile}
+                                    >
+                                        Change Avatar
+                                    </Button>
+                                </div>
+                            </div>
+                            <FormField
+                                control={profileForm.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Full Name</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <div className="grid gap-2">
+                                <Label htmlFor="email">Email Address</Label>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    defaultValue={user?.email}
+                                    disabled
+                                />
+                                <p className="text-xs text-muted-foreground">
+                                    Your email address cannot be changed.
+                                </p>
+                            </div>
+                            <Button
+                                type="submit"
+                                className="w-fit"
+                                disabled={isUpdatingProfile || isPending}
+                            >
+                                {isUpdatingProfile ? (
+                                    <>
+                                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                        Saving...
+                                    </>
+                                ) : (
+                                    "Save Changes"
+                                )}
+                            </Button>
+                        </form>
+                    </FormProvider>
+                </CardContent>
+            </Card>
+
+            <Card>
+                <CardHeader>
+                    <CardTitle>Security</CardTitle>
+                    <CardDescription>Update your password.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <FormProvider {...form}>
+                        <form onSubmit={form.handleSubmit(handlePasswordChange)} className="grid gap-4">
+                            {error && (
+                                <Alert variant="destructive">
+                                    <AlertCircle className="h-4 w-4" />
+                                    <AlertDescription>{error.message}</AlertDescription>
+                                </Alert>
+                            )}
+
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="currentPassword"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Current Password</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                id="current-password"
+                                                type="password"
+                                                placeholder="********"
+                                                {...field}
+                                                />
+                                            </FormControl>
+                                        <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <FormField
+                                    control={form.control}
+                                    name="newPassword"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>New Password</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                id="new-password"
+                                                type="password"
+                                                placeholder="********"
+                                                {...field}
+                                                />
+                                            </FormControl>
+                                        <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+
+                                <FormField
+                                    control={form.control}
+                                    name="confirmPassword"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Confirm Password</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                id="confirm-password"
+                                                placeholder="********"
+                                                type="password"
+                                                {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <Button
+                                type="submit"
+                                className="mt-2 w-fit"
+                                disabled={isPending || isChangingPassword}
+                            >
+                                {isPending || isChangingPassword ? (
+                                    <>
+                                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                                        Updating...
+                                    </>
+                                ) : (
+                                    "Update Password"
+                                )}
+                            </Button>
+                        </form>
+                    </FormProvider>
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
